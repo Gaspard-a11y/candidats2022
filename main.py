6b38ec325acfe864a0bdc4d0781e839a188c6267
Gaspard-a11y <gaspard.sagot@gmail.com>
2022-03-31 15:48:54 +0200

    Rename programmes into candidats

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ import random
 
 import fire
 
-programmes_folder = '.\programmes'
+programmes_folder = '.\candidats'
 
 
 def load_json(json_path):
-    return json.load(open(json_path,"rb"), encoding="utf-8")
+    return json.load(open(json_path, "rb"), encoding="utf-8")
 
 
 def main(candidats='all'):
@@ -26,7 +26,7 @@ def main(candidats='all'):
 
     programme_paths = [os.path.join(programmes_folder, programmes_files)
                        for programmes_files in os.listdir(programmes_folder)]
-    
+
     # Build dict str proposition -> str candidate name
     propositions_dico = {}
     for programme_path in programme_paths:
@@ -41,30 +41,35 @@ def main(candidats='all'):
 
     # Ask the questions
     # FIXME overkill
-    score_by_candidate = {propositions_dico[proposition] : 0 for proposition in propositions_dico.keys()}
+    score_by_candidate = {
+        propositions_dico[proposition]: 0 for proposition in propositions_dico.keys()}
 
     for i, proposition in enumerate(proposition_keys):
-        print(f"-------------------- Proposition {i} sur {num_propositions} --------------------")
+        print(
+            f"-------------------- Proposition {i} sur {num_propositions} --------------------")
         print(proposition)
         print("\n")
         candidate_name = propositions_dico[proposition]
-        importance = input("Importance de la mesure (mesure inutile : 0 , ... , 5 : mesure indispensable) : ")
-        accord = input("Accord avec la mesure (pas du tout d'accord : -5 , ... , 5 : complètement d'accord) : ")
+        importance = input(
+            "Importance de la mesure (mesure inutile : 0 , ... , 5 : mesure indispensable) : ")
+        accord = input(
+            "Accord avec la mesure (pas du tout d'accord : -5 , ... , 5 : complètement d'accord) : ")
         score = float(importance)*float(accord)/25
-        score_by_candidate[candidate_name]+=score
+        score_by_candidate[candidate_name] += score
         print("\n")
 
     # Print results
     print("\n")
     print("---------------------------------- Résultats ----------------------------------")
-    print(f"Voici les scores de {name} par candidats (complètement en désaccord : -1 , ... , 1 : complètement en accord)")
+    print(
+        f"Voici les scores de {name} par candidats (complètement en désaccord : -1 , ... , 1 : complètement en accord)")
     print("\n")
 
     for candidate_name in list(score_by_candidate.keys()):
         print(f"Candidat : {candidate_name}")
         print(f"score : {score_by_candidate[candidate_name]/20}")
         print("\n")
-   
+
     # TODO allow saving the results in a e.g. txt file
     # TODO add matplotlib dependency and save a plot
 
