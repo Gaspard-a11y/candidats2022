3bfbbdc8b2480dec1314fd39f6c26d227286d100
Gaspard-a11y <gaspard.sagot@gmail.com>
2022-03-31 17:06:08 +0200

    Fix candidates' nicknames

--- candidat.py
+++ candidat.py
@@ -5,7 +5,7 @@ class Candidat():
     def __init__(self, path_to_json):
         dico = json.load(open(path_to_json, "rb"), encoding="utf-8")
         self.name = dico["name"]
-        self.nickname = dico["nickname"]
+        self.nickname = dico["nickname"].lower()
         self.parti = dico["parti"]
         self.site = dico["site"]
         self.propositions = dico["propositions"]
