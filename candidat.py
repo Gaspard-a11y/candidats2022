7017d7acd535d0c635254cd4754b001aab347507
Gaspard-a11y <gaspard.sagot@gmail.com>
2022-03-31 15:52:11 +0200

    WIP Add Candidats class.

--- candidat.py
+++ candidat.py
@@ -3,7 +3,7 @@ import json
 
 class Candidat():
     def __init__(self, path_to_json):
-        dico = json.load(open(json_path, "rb"), encoding="utf-8")
+        dico = json.load(open(path_to_json, "rb"), encoding="utf-8")
         self.name = dico["name"]
         self.nickname = dico["nickname"]
         self.parti = dico["parti"]
